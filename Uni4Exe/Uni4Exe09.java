957b0e42029be1dc441deeb29636dcf7c131bb67
Juliano Araújo Cezarotto <145863641+Juliano1504@users.noreply.github.com>
2024-09-13 22:25:44 -0300

    Update Uni4Exe09.java

--- Uni4Exe/Uni4Exe09.java
+++ Uni4Exe/Uni4Exe09.java
@@ -8,16 +8,16 @@ public class Uni4Exe09 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Se o número: ");
+        System.out.println("Valor 1: ");
         int n1 = Integer.parseInt(teclado.nextLine());
 
-        System.out.println("É múltiplo de: ");
+        System.out.println("Valor 2 ");
         int n2 = Integer.parseInt(teclado.nextLine());
 
-        if (n1 % n2 == 0) {
-            System.out.println("É múltiplo");
+        if (n1 % n2 == 0 || n2 % n1 == 0) {
+            System.out.println("Um dos números é múltiplo do outro");
         } else {
-            System.out.println("Não é múltiplo");
+            System.out.println("Nenhum dos números é múltiplo do outro");
         }
         teclado.close();
     }
